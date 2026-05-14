59740951a16721fb0e3752d5b9ad01b48c3c3b5a
Zyragoon <Zyragoon@gmail.com>
2026-05-13 20:08:57 -0600

    Initial commit

---
+++ src/main/java/zyra/armor_reforged/ArmorReforgedClient.java
@@ -0,0 +1,10 @@
+package zyra.armor_reforged;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArmorReforgedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/zyra/armor_reforged/ArmorReforgedDataGenerator.java
@@ -0,0 +1,11 @@
+package zyra.armor_reforged;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ArmorReforgedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
